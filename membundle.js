3fc91b9fe97df77cfac7951404529733bea20596
crp222 <87718115+crp222@users.noreply.github.com>
2023-08-31 15:10:12 +0200

    Add files via upload

--- membundle.js
+++ membundle.js
@@ -1,21 +1,15 @@
 const { rollup } = require("rollup");
 const fs = require("fs");
 const commonjs = require("@rollup/plugin-commonjs")
-const resolve = require("@rollup/plugin-node-resolve");
-const replace = require("@rollup/plugin-replace");
 const babel = require("@rollup/plugin-babel");
 const {getFiles, getFileExtension,copy} = require("./utils.js");
+
+var files = [];
 
 const inputOptions = {
     input : {},
     plugins : [
-        resolve(),
         commonjs(),
-        replace({
-            'process.env.NODE_ENV': JSON.stringify('development'),
-            __buildDate__: () => JSON.stringify(new Date()),
-            preventAssignment:true,
-        }),
         babel({ 
             presets: ['@babel/preset-react'],
             babelHelpers:"bundled",
@@ -26,38 +20,50 @@ const inputOptions = {
 }
 
 const outputOptions = {
-    dir : "build",
     format : "es",
-    preserveModules : true,
 }
 
 function getInputFiles() {
-    fs.rmSync("build",{recursive:true});
-    fs.mkdirSync("build");
     inputOptions.input = {};
-    let files = [];
-    getFiles("src",files);
-    files.forEach(f => {
+    files = [];
+    let buff = [];
+    getFiles("src",buff);
+    buff.forEach(f => {
         if(["js","jsx"].includes(getFileExtension(f)))
             inputOptions.input[f.replace("src/","").split(".")[0]] = f;
         else {
             let buildf = f.replace("src","build")
-            copy(f,buildf);
+            let content = fs.readFileSync(f,"utf-8");
+            files.push({path:buildf,content:content,type:"asset"});
         }
     })
 }
 
-async function build() {
-    console.log("Building...");
+function saveBundledFiles(output) {
+    output.forEach(o => {
+        files.push({path:"build/"+o.fileName,content:o.code,type:"javascript"});
+    })
+}
+
+async function bundleInMemory() {
+    console.log("Building in memory...");
     getInputFiles();
     let bundle;
     try {
         bundle = await rollup(inputOptions);
-        await bundle.write(outputOptions);
-        console.log("Build finished!");
+        let rollupOutput = await bundle.generate(outputOptions);
+        saveBundledFiles(rollupOutput.output);
+        console.log("Build in memory finished!");
     }catch(err){
         console.log(err);
     }
 }
 
-module.exports = {build};
+function getMemorySavedFiles() {
+    return files;
+}
+
+module.exports = {
+    build : bundleInMemory,
+    getFiles : getMemorySavedFiles,
+}
--- server.js
+++ server.js
@@ -1,70 +1,80 @@
 const express = require('express');
 const app = express();
 const bundle = require("./bundler.js");
+const memBundle = require("./membundle.js");
 const path = require("path");
 const {getFiles} = require("./utils.js");
 const fs = require("fs");
 
 const DEVELOPMENT = true;
 
+var buildDirFiles = [];
+
 if(DEVELOPMENT){
     app.get("/",(req,res)=>{
-        bundle.build().then(()=>{
-            fixImports();
-            res.sendFile(path.join(__dirname,"build/index.html"));
+        memBundle.build().then(()=>{
+            res.send(memBundle.getFiles().find(file => file.path == "build/index.html").content);
         });
+    })
+
+    app.get("*",(req,res)=>{
+        let url = "build"+req.url;
+        let memFile = memBundle.getFiles().find(file => file.path == url);
+        if(memFile){
+            let content = memFile.content;
+            if(memFile.type == "javascript"){
+                content = fixImportPath(content);
+                res.setHeader('Content-Type', 'application/javascript; charset=utf-8');
+            }
+            res.send(`${content}`);
+            return;
+        }
+        if(buildDirFiles.includes(url))
+            res.sendFile(path.join(__dirname,url));
     })
 }else {
     app.get("/",(req,res)=>{
         res.sendFile(path.join(__dirname,"build/index.html"));
     })
-}
 
-var files = [];
-app.get("*",(req,res)=>{
-    let url = "build"+req.url;
-    if(files.includes(url))
-        res.sendFile(path.join(__dirname,url));
-})
-
-function fixImports() {
-    files.forEach(file => {
-        if(file.includes("node_modules")){
-            return;
-        }
-        let content = fs.readFileSync(file,"utf-8");
-        let lines = content.split("\n");
-        lines.forEach((line,i) => {
-            if(line.trim().startsWith("import")){
-                // fix import path
-                let module = line.split("'")[1];
-                module = module.split("'")[0];
-                if(!module.startsWith(".")){
-                    let matchingModules = files.filter(file => file.includes("/"+module));
-                    let modulePath = "";
-                    if(matchingModules.length == 1){
-                        modulePath  = matchingModules[0].replace("build/","");
-                    }else {
-                        modulePath = "node_modules/"+module+"/index.js";
-                    }
-                    lines[i] = line.replace(module,"/"+modulePath);
-                }
-                // TODO : fix import react functions "{ useState, useEffect } => { r as reactExports }"
-            }
-        })
-        content = lines.join("\n");
-        fs.writeFileSync(file,content);
+    app.get("*",(req,res)=>{
+        let url = "build"+req.url;
+        if(buildDirFiles.includes(url))
+            res.sendFile(path.join(__dirname,url));
     })
 }
 
 async function run() {
     await bundle.build();
     if(DEVELOPMENT) 
-        await bundle.build();
-    files = getFiles("build");
-    fixImports();
+        await memBundle.build();
+        buildDirFiles = getFiles("build");
     app.listen(3000,()=>{
         console.log("Server is listening on port 3000");
     });
 }
 run();
+
+
+function fixImportPath(content) {
+    let lines = content.split("\n");
+    lines.forEach((line,i) => {
+        if(line.trim().startsWith("import")){
+            // fix import path
+            let module = line.split("'")[1];
+            module = module.split("'")[0];
+            if(!module.startsWith(".")){
+                let matchingModules = buildDirFiles.filter(file => file.includes("/"+module));
+                let modulePath = "";
+                if(matchingModules.length == 1){
+                    modulePath  = matchingModules[0].replace("build/","");
+                }else {
+                    modulePath = "node_modules/"+module+"/index.js";
+                }
+                lines[i] = line.replace(module,"/"+modulePath);
+            }
+            // TODO : fix import react functions "{ useState, useEffect } => { r as reactExports }"
+        }
+    })
+    return lines.join("\n");
+}
