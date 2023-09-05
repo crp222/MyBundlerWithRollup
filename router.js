af3d9eed1b4e9912e78ae3cb617ee90b4f791b35
crp222 <87718115+crp222@users.noreply.github.com>
2023-09-05 17:57:49 +0200

    Add files via upload

--- router.js
+++ router.js
@@ -5,18 +5,8 @@ const memBundle = require('./mybundler/membundle.js');
 
 const routes =
 {
-    "/":"home/home.html",
+    "/":"index.html",
 
 }
 
-// Making  the routes
-Object.entries(routes).forEach(e=>{
-    const [k,v] = e;
-    router.get(k,(req,res)=>{
-        memBundle.build().then(()=>{
-            res.sendFile(path.join(__dirname,"build",v));
-        });
-    })  
-})
-
-module.exports = router;
+module.exports = routes;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bundle = require("./mybundler/bundler.js");
 const memBundle = require("./mybundler/membundle.js");
 const path = require("path");
 const {getFiles,fixImportPath} = require("./mybundler/utils.js");
-const customRouter = require("./router.js");
+const routes = require("./router.js");
 const fs = require("fs");
 
 ///////////////////////////////////////////
@@ -12,14 +12,23 @@ const fs = require("fs");
 const DEVELOPMENT = true;
 ///////////////////////////////////////////
 
-app.use(customRouter);
-
 var buildDirFiles = [];
 if(DEVELOPMENT){
-    app.get("/",(req,res)=>{
-        memBundle.build().then(()=>{
-            res.send(memBundle.getFiles().find(file => file.path == "build/index.html").content);
-        });
+
+    // Making the routes 
+    // NOT TESTED
+    Object.entries(routes).forEach(e=>{
+        const [k,v] = e;
+        app.get(k,(req,res)=>{
+            let memFile = memBundle.getFiles().find(file => path.normalize(file.path) == path.join("build",v));
+            if(memFile){
+                memBundle.build().then(()=>{
+                    res.send(`${memFile.content}`);
+                });
+                return;
+            }
+            res.sendStatus(404);
+        })  
     })
 
     // static serving
@@ -28,7 +37,7 @@ if(DEVELOPMENT){
         if(!url.startsWith("build")) {
             url = "build"+url;
         }
-        let memFile = memBundle.getFiles().find(file => file.path == url);
+        let memFile = memBundle.getFiles().find(file => path.normalize(file.path) == path.normalize(url));
         if(memFile){
             let content = memFile.content;
             if(memFile.type == "javascript"){
@@ -55,10 +64,14 @@ if(DEVELOPMENT){
 
     
 }else {
-    app.get("/",(req,res)=>{
-        res.sendFile(path.join(__dirname,"build/index.html"));
+    Object.entries(routes).forEach(e=>{
+        const [k,v] = e;
+        app.get(k,(req,res)=>{
+            memBundle.build().then(()=>{
+                res.sendFile(path.join("build",v));
+            });
+        })  
     })
-
     app.get("*",(req,res)=>{
         let url = "build"+req.url;
         if(buildDirFiles.includes(url))
@@ -70,7 +83,7 @@ async function run() {
     await bundle.build();
     if(DEVELOPMENT) 
         await memBundle.build();
-        buildDirFiles = getFiles("build");
+    buildDirFiles = getFiles("build");
     app.listen(3000,()=>{
         console.log("Server is listening on port 3000");
     });
